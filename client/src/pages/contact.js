767153d9c733e44a74e939346ccae59708d59229
David Sackettgit config --global user.name David <davidsackett14@gmail.com>
2021-06-19 23:50:57 -0600

    added page layout, added react-router, install bootstrap

---
+++ client/src/pages/contact.js
@@ -0,0 +1,4 @@
+import React from "react";
+export default function ContactPage() {
+  return <div>words</div>;
+}
---
+++ client/src/pages/projects.js
@@ -0,0 +1,4 @@
+import React from "react";
+export default function ProjectPage() {
+  return <div>words</div>;
+}
