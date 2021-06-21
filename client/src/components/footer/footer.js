3c3e70b49182db2d42e800b40532880e7eb4c7e1
David Sackettgit config --global user.name David <davidsackett14@gmail.com>
2021-06-20 18:10:06 -0600

    change styling and orientation of page links, added link to call, add link to resume, added resume link and pdf

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -3,70 +3,72 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-      <div>  
-          
-            <footer
-      className="text-center text-white"
-      style={{ backgroundColor: "f1f1f1" }}
-    >
-       
-      <div className="container pt-4">
-      <section className="mb-4">
-      <h6 className="text-center text-black">Please visit me at Instagram, LinkedIn, GitHub, or reach out by E-mail</h6>
-      <div
-        className="text-center text-dark p-3"
-        style={{ backgroundColor: (0, 0, 0, 0.2) }}
+    <div>
+      <footer
+        className="text-center text-white"
+        style={{ backgroundColor: "f1f1f1" }}
       >
-       Please visit me at Instagram, LinkedIn, GitHub, or reach out by E-mail
-       
-      </div>
-      </section>
-        <section className="mb-4">
-          <Link
-            className="btn btn-link btn-floating btn-lg text-dark m-1"
-            to="https://www.instagram.com/dsi_hvac/"
-            role="button"
-            data-mdb-ripple-color="dark"
-          >
-            <i className="fa fa-instagram"></i>
-          </Link>
-          <Link
-            className="btn btn-link btn-floating btn-lg text-dark m-1"
-            to="https://www.linkedin.com/in/david-sackett-5324a911b/"
-            role="button"
-            data-mdb-ripple-color="dark"
-          >
-            <i className="fa fa-linkedin"></i>
-          </Link>
-          <Link
-            className="btn btn-link btn-floating btn-lg text-dark m-1"
-            to="https://github.com/davidsackett14"
-            role="button"
-            data-mdb-ripple-color="dark"
-          >
-            <i className="fa fa-github"></i>
-          </Link>
-          <a
-            target="_blank"
-            href="mailto:david@dsihvac.com"
-            className="link-primary btn btn-link btn-floating btn-lg text-dark m-1"
-          >
-            <i className="fa fa-envelope"></i>
+        <div className="container pt-4">
+          <section className="mb-4">
+            <h6 className="text-center text-black">
+              Please visit me at Instagram, LinkedIn, GitHub, or reach out by
+              E-mail
+            </h6>
+            <div
+              className="text-center text-dark p-3"
+              style={{ backgroundColor: (0, 0, 0, 0.2) }}
+            >
+              Please visit me at Instagram, LinkedIn, GitHub, or reach out by
+              E-mail
+            </div>
+          </section>
+          <section className="mb-4">
+            <Link
+              className="btn btn-link btn-floating btn-lg text-dark m-1"
+              to="https://www.instagram.com/dsi_hvac/"
+              role="button"
+              data-mdb-ripple-color="dark"
+            >
+              <i className="fa fa-instagram"></i>
+            </Link>
+            <Link
+              className="btn btn-link btn-floating btn-lg text-dark m-1"
+              to="https://www.linkedin.com/in/david-sackett-5324a911b/"
+              role="button"
+              data-mdb-ripple-color="dark"
+            >
+              <i className="fa fa-linkedin"></i>
+            </Link>
+            <Link
+              className="btn btn-link btn-floating btn-lg text-dark m-1"
+              to="https://github.com/davidsackett14"
+              role="button"
+              data-mdb-ripple-color="dark"
+            >
+              <i className="fa fa-github"></i>
+            </Link>
+            <a
+              target="_blank"
+              href="mailto:david@dsihvac.com"
+              className="link-primary btn btn-link btn-floating btn-lg text-dark m-1"
+              rel="noreferrer"
+            >
+              <i className="fa fa-envelope"></i>
+            </a>
+          </section>
+        </div>
+
+        <div
+          className="text-center text-dark p-3"
+          style={{ backgroundColor: (0, 0, 0, 0.2) }}
+        >
+          © 2021 Copyright:
+          <a className="text-dark" href="mailto:david@dsihvac.com">
+            David Sackett
           </a>
-        </section>
-      </div>
-
-      <div
-        className="text-center text-dark p-3"
-        style={{ backgroundColor: (0, 0, 0, 0.2) }}
-      >
-        © 2021 Copyright:
-        <a className="text-dark" href="mailto:david@dsihvac.com">
-          David Sackett
-        </a>
-      </div>
-    </footer></div>
-
+        </div>
+      </footer>
+    </div>
   );
 }
 
--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -1,22 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import AllPagesPDFViewer from "../../components/pdf/all-pages";
+import samplePDF from "../pdf/resume.pdf"
 function Header() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <span className="navbar-brand">David Sackett</span>
-
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/projects">Projects</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact Me</Link>
-        </li>
-      </ul>
+      <span className="navbar-brand">
+        <Link to="/">About</Link>
+      </span>
+      <span className="navbar-brand">
+        <Link to="/projects">Projects</Link>
+      </span>
+      <span className="navbar-brand">
+      <div className="all-page-container">
+        <a href={<AllPagesPDFViewer pdf={samplePDF}/>}>resume</a>
+      </div>
+      </span>
+      
+      <span className="navbar justify-content-right ">
+              <a href="tel:+1-555-555-1212">555-555-1212</a>
+      </span>
+     
     </nav>
   );
 }
