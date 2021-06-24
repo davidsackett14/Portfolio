ec5d0be16a6769443a233a2ea166e2e92ee4e83e
David Sackettgit config --global user.name David <davidsackett14@gmail.com>
2021-06-24 09:07:53 -0600

    update cards and add images to image file

--- client/src/components/projectcard/projectCard.js
+++ client/src/components/projectcard/projectCard.js
@@ -1,51 +1,50 @@
 import React from "react";
+// const images = require("../../images");
 
 function Project(props) {
   return (
- 
-
-
-
-
-    <div className="row" style={{padding:"3px", backgroundColor:"lightgray"}}>
-       <div className="col-sm-6">
-    <div className="card">
-      <div className="card-body">
-      <img className="card-img-top" src={props.image} alt="Card cap" />
+    <div
+      className="row"
+      style={{ padding: "3px", backgroundColor: "lightgray" }}
+    >
+      <div className="col-sm-6">
+        <div className="card">
+          <div className="card-body">
+            <img
+              className="card-img-top"
+              src={props.image}
+              alt="Card cap"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="col-sm-6">
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">{props.title}</h5>
+            <p className="card-text">{props.desc}</p>
+            <a
+              href={props.deploy}
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-primary"
+              style={{ margin: "5px" }}
+            >
+              Go to deployed app
+            </a>
+            <a
+              href={props.repo}
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-primary"
+              style={{ margin: "5px" }}
+            >
+              Go to repository to view code
+            </a>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-  <div className="col-sm-6">
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">{props.title}</h5>
-        <p className="card-text">{props.desc}</p>
-        <a
-          href={props.deploy}
-          target="_blank"
-          rel="noreferrer"
-          className="btn btn-primary"
-          style={{ margin: "5px" }}
-        >
-          Go to deployed app
-        </a>
-        <a
-          href={props.repo}
-          target="_blank"
-          rel="noreferrer"
-          className="btn btn-primary"
-          style={{ margin: "5px" }}
-        >
-          Go to repository to view code
-        </a>
-      </div>
-    </div>
-  </div>
- 
-</div>
-  
-      
-      
   );
    
     
