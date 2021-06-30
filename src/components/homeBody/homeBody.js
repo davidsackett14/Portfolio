17d138368faf257836b2eba8724a79765929cbe9
David Sackettgit config --global user.name David <davidsackett14@gmail.com>
2021-06-29 19:01:33 -0600

    change profile picture to link

--- src/components/homeBody/homeBody.js
+++ src/components/homeBody/homeBody.js
@@ -6,38 +6,34 @@ export default function AboutMe() {
   return (
     <div>
       <div
-      
-      
-      
-           className="clearfix  p-4 text-dark"
-    
-    
-    
-                 style={{    backgroundColor:    "silver"    }}
-      
-      
-      
+        className="clearfix  p-4 text-dark"
+        style={{ backgroundColor: "silver" }}
       >
         <img
-          src={profilePic}
+          src="https://raw.githubusercontent.com/davidsackett14/Portfolio/main/src/images/rodin.jpg"
           className="col-md-2 float-md-end mb-3 ms-md-3"
           alt="..."
         />
 
         <p>
-          <i style={{fontFamily:"cursive", fontWeight:"bold", fontSize:22,}}>Hello</i>, let me introduce myself. I am a student of life and in
-          this season of learning I am exploring the world of web development. I
-          have ran an HVAC company for the last 5 of my 12 years in the
-          industry. I love architecture, building, and creation which has drawn
-          me to the development of web apps. A way to build tangible things
-          where the limits are only in one's mind.
+          <i
+            style={{ fontFamily: "cursive", fontWeight: "bold", fontSize: 22 }}
+          >
+            Hello
+          </i>
+          , let me introduce myself. I am a student of life and in this season
+          of learning I am exploring the world of web development. I have ran an
+          HVAC company for the last 5 of my 12 years in the industry. I love
+          architecture, building, and creation which has drawn me to the
+          development of web apps. A way to build tangible things where the
+          limits are only in one's mind.
         </p>
 
         <p>
           I am currently attending the University of Utah's coding bootcamp
           program. I am familiar with and have used the following languages and
           technologies:
-          <p style={{fontSize:17, fontWeight:7}}>
+          <p style={{ fontSize: 17, fontWeight: 7 }}>
             <div className="row">
               <ul className="col-sm-4 p-2">
                 <li>HTML</li> <li>CSS</li>
